ce62ef3b24f9f38949161e857014130a4d303f8d
Tod E. Kurt <tod@todbot.com>
2020-06-23 14:05:35 -0700

    add description in main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+/**
+ * electron-hid-test main.js
+ * github.com/todbot/electron-hid-test
+ *
+ * Demonstrate node-hid use in Electron renderer and Electron main
+ */
+
 const { app, BrowserWindow } = require('electron')
 
 var HID = require('node-hid')
@@ -57,4 +64,3 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
